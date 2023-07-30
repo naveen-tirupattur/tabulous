7d9e54d2c14fb7321c5e13eac820765d974b6a2d
Naveen Tirupattur <naveen.tirupattur@workday.com>
2023-07-29 21:49:05 -0700

    Refactor and redesign UI

--- background.js
+++ background.js
@@ -13,7 +13,6 @@ function groupTabsByDomain(tabs) {
       tabsByDomain.set(domain, [tab.id]);
     }
   });
-  console.log(tabsByDomain);
   return tabsByDomain;
 }
 
@@ -24,15 +23,21 @@ async function groupTabsToWindow(tabIds, windowId, domain) {
   // await chrome.tabs.move(tabIds, { windowId, index: -1 });
 }
 
+// Group Tabs Handler
+function groupTabsHandler(selectedElement, selectedAction) {
+  if (selectedAction) {
+    groupTabs(selectedElement)
+  } else {
+    ungroupTabs(selectedElement)
+  }
+}
+
 // Function to handle grouping tabs
-function groupTabsHandler(tabGroupingOption) {
+function groupTabs(tabGroupingOption) {
   const windowId = tabGroupingOption === 'groupActive' ? chrome.windows.WINDOW_ID_CURRENT : chrome.windows.WINDOW_ID_NONE;
-  chrome.tabs.query({windowId:windowId}, (tabs) => {
+  chrome.tabs.query({windowId: windowId}, (tabs) => {
     const tabGroups = groupTabsByDomain(tabs);
-    // var sortedMap = [tabGroups.entries()].sort((a, b) => a[1] > b[1]);
     tabGroups.forEach(function (tabIds, domain) {
-      console.log("Domain:", domain);
-      console.log("Tabs:", tabIds);
       if (tabIds.length > 1) {
         groupTabsToWindow(tabIds, windowId, domain);
       }
@@ -41,9 +46,9 @@ function groupTabsHandler(tabGroupingOption) {
 }
 
 // Function to handle ungrouping of tabs in active window
-function ungroupTabsHandler(tabGroupingOption) {
-  console.log(tabGroupingOption);
-  const windowId = tabGroupingOption === 'ungroupActive' ? chrome.windows.WINDOW_ID_CURRENT : chrome.windows.WINDOW_ID_NONE;
+function ungroupTabs(tabGroupingOption) {
+  const windowId = tabGroupingOption === 'groupActive' ? chrome.windows.WINDOW_ID_CURRENT : chrome.windows.WINDOW_ID_NONE;
+  console.log('Ungrouping Tabs', windowId);
   chrome.tabs.query({windowId:windowId}, (tabs) => {
     tabs.forEach((tab) => {
       chrome.tabs.ungroup(tab.id);
@@ -51,12 +56,71 @@ function ungroupTabsHandler(tabGroupingOption) {
   });
 }
 
+const tabUrls = new Set();
+const duplicateTabs = new Set();
+// chrome.tabs.onRemoved.addListener((tabId) => {
+//   tabIds.delete(tabId);
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status === 'complete') {
+//     const duplicateTab = findDuplicateTab(tab);
+//     if (duplicateTab) {
+//       notifyDuplicateTab(duplicateTab);
+//     } else {
+//       tabIds.add(tabId);
+//     }
+//   }
+// });
+
+// Function to detect duplicate tabs in either active window or all windows
+function duplicateTabsHandler(selectedElement, selectedAction) {
+  if (selectedAction) {
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        if (tabUrls.has(tab.url)) {
+          // Handle duplicate tab here, for example, group them or provide visual feedback
+          // For simplicity, we'll just display an alert message
+          // console.log('Found duplicate tab', tab.url)
+          duplicateTabs.add(tab);
+        } else {
+          tabUrls.add(tab.url);
+        }
+      });
+    });
+    // groupAllDuplicateTabs();
+  }
+}
+function groupAllDuplicateTabs() {
+  const groups = new Map();
+  duplicateTabs.forEach((tab) => {
+    const key = tab.url;
+    if (!groups.has(key)) {
+      groups.set(key, [tab]);
+    } else {
+      groups.get(key).push(tab);
+    }
+  });
+
+  const groupedTabs = [];
+  groups.forEach((group) => {
+    if (group.length > 1) {
+      groupedTabs.push(group);
+    }
+  });
+
+  sendGroupedTabsToPopup(groupedTabs);
+}
+
+function sendGroupedTabsToPopup(groupedTabs) {
+  chrome.runtime.sendMessage({ action: 'updateDuplicateTabs', duplicates: groupedTabs });
+}
+
 // Listen for messages from the popup window
 chrome.runtime.onMessage.addListener((request, sender,  sendResponse) => {
-  console.log(request);
-  if (request.optionValue === 'groupActive' || request.optionValue === 'groupAll') {
-    groupTabsHandler(request.optionValue);
-  } else if (request.optionValue === 'ungroupActive' || request.optionValue === 'ungroupAll') {
-    ungroupTabsHandler(request.optionValue);
+  if (request.handler === 'groupTabs') {
+    groupTabsHandler(request.selectedElement, request.selectedAction);
+  } else if (request.handler === 'detectDuplicates') {
+    duplicateTabsHandler(request.selectedElement, request.selectedAction);
   }
 });
--- popup.js
+++ popup.js
@@ -1,27 +1,74 @@
 // popup.js
+
 // Function to send a message to background.js
-function sendMessageToBackground(action, selectedGroups) {
-  chrome.runtime.sendMessage({ action, selectedGroups });
+function sendMessageToBackground(handler, selectedElement, selectedAction) {
+  chrome.runtime.sendMessage({ handler, selectedElement, selectedAction });
 }
 
-// Function to handle changes in the toggle switches (radio buttons)
-function handleToggleSwitches() {
-
-  const selectedAction = document.querySelector('input[name="tabAction"]:checked').value;
-  if (selectedAction === 'groupActive' || selectedAction === 'groupAll') {
-    chrome.runtime.sendMessage({ action: 'groupTabs', optionValue: selectedAction });
-  } else if (selectedAction === 'ungroupActive' || selectedAction === 'ungroupAll') {
-    chrome.runtime.sendMessage({ action: 'ungroupTabs', optionValue: selectedAction });
+// Function to handle changes in the toggle switches
+function handleToggleSwitches(selectedElement, selectedAction) {
+  // const selectedAction = document.querySelector('input[name="tabAction"]:checked').value;
+  if (selectedElement === 'groupActive' || selectedElement === 'groupAll') {
+    sendMessageToBackground('groupTabs', selectedElement, selectedAction);
+  } else if (selectedElement === 'detectAll') {
+    sendMessageToBackground('detectDuplicates', selectedElement, selectedAction);
   }
+  // Save the value to sync storage
+  chrome.storage.sync.set({[selectedElement]: selectedAction}, function() {
+    console.log('Saved', selectedElement, selectedAction);
+  });
 }
 
-// Listen for changes in the toggle switches (radio buttons)
-const toggleSwitches = document.querySelectorAll('input[name="tabAction"]');
-toggleSwitches.forEach(switchElement => {
-  switchElement.addEventListener('change', handleToggleSwitches);
+document.addEventListener('DOMContentLoaded', function() {
+  // Listen for changes in the toggle switches (radio buttons)
+  const toggleSwitches = document.querySelectorAll('input[name="tabAction"]');
+  toggleSwitches.forEach(switchElement => {
+    const selectedElement = switchElement.value;
+    // Load the previous state from sync storage
+    chrome.storage.sync.get(selectedElement, function(result) {
+      console.log(selectedElement, result[selectedElement])
+      switchElement.checked = result[selectedElement];
+      if(selectedElement === 'groupActive' || selectedElement === 'detectAll')
+        handleToggleSwitches(selectedElement, result[selectedElement]);
+    });
+
+    // Listen to switch value changes
+    switchElement.addEventListener('change', function (){
+      const selectedAction = switchElement.checked;
+      handleToggleSwitches(selectedElement, selectedAction);
+    });
+  });
+
+  const groupAllButton = document.getElementById('groupAll');
+
+  groupAllButton.addEventListener('click', function () {
+    handleToggleSwitches('groupAll', true);
+  });
+
+  // Function to update the popup content with the list of duplicate tabs
+  function updatePopupContent(duplicateTabs) {
+    const duplicateTabsList = document.getElementById('duplicateTabsList');
+    duplicateTabsList.innerHTML = '';
+
+    if (duplicateTabs.length === 0) {
+      duplicateTabsList.textContent = 'No duplicate tabs found.';
+    } else {
+      duplicateTabsList.innerHTML = ''; // Clear existing content
+      duplicateTabs.forEach((group) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = group[0].title; // Display the title of the first tab in the group
+        listItem.classList.add('duplicate-tab-group'); // Add the CSS class to the list item
+        duplicateTabsList.appendChild(listItem);
+      });
+    }
+  }
+
+  // Handle messages from background script
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'updateDuplicateTabs') {
+      updatePopupContent(message.duplicates);
+    }
+  });
 });
 
-// Listen for messages from background.js
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // (No changes required for this part)
-});
+
